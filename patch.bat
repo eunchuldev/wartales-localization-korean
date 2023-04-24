2df767348b2b580522d1eb68c2eda34bf9c766af
Eunchul <eunchulsong9@gmail.com>
2023-04-24 17:29:11 +0900

    correctly backup original assets

--- patch.bat
+++ patch.bat
@@ -1,14 +1,28 @@
-IF NOT EXIST "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak.back" (
-  copy "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak" "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak.back"
-)
-IF NOT EXIST "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak.back" (
-  copy "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak" "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak.back"
-)
+@echo off
+
+SET WARTALE_HOME=C:\Program Files (x86)\Steam\steamapps\common\Wartales
+SET LAST_PATCHED_TRACKER="./LAST_PATCHED_AT"
+
+SET LAST_PATCHED=null
+SET /p LAST_PATCHED=<%LAST_PATCHED_TRACKER%
+
+FOR %%a IN ("%WARTALE_HOME%\res.pak") do set LAST_MODIFIED=%%~ta || EXIT /B 1
+
+IF "%LAST_PATCHED%" NEQ "%LAST_MODIFIED%" (
+  echo last patched at %LAST_PATCHED%, but target file is updated at %LAST_MODIFIED%
+  echo It might first time patch or new update arrived
+  copy "%WARTALE_HOME%\assets.pak" "%WARTALE_HOME%\assets.pak.back"
+  copy "%WARTALE_HOME%\res.pak" "%WARTALE_HOME%\res.pak.back"
+) || EXIT /B 1
+
 IF NOT EXIST quickbms (
   curl -O http://aluigi.altervista.org/papers/quickbms.zip
   mkdir quickbms
   tar -C quickbms -xf quickbms.zip
-)
+) || EXIT /B 1
 
-quickbms\quickbms_4gb_files.exe -w -r -r dune_spice_wars_extract.bms "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak" asset
-quickbms\quickbms.exe -w -r -r dune_spice_wars_extract.bms "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak" res
+quickbms\quickbms_4gb_files.exe -w -r -r dune_spice_wars_extract.bms "%WARTALE_HOME%\assets.pak" asset || EXIT /B 1
+quickbms\quickbms.exe -w -r -r dune_spice_wars_extract.bms "%WARTALE_HOME%\res.pak" res || EXIT /B 1
+
+FOR %%a IN ("%WARTALE_HOME%\res.pak") do set LAST_PATCHED=%%~ta
+echo %LAST_PATCHED% > %LAST_PATCHED_TRACKER%
--- revert.bat
+++ revert.bat
@@ -1,2 +1,9 @@
-move \y "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak.back" "C:\Program Files (x86)\Steam\steamapps\common\Wartales\assets.pak"
-move \y "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak.back" "C:\Program Files (x86)\Steam\steamapps\common\Wartales\res.pak"
+@echo off
+
+SET WARTALE_HOME=C:\Program Files (x86)\Steam\steamapps\common\Wartales
+SET LAST_PATCHED_TRACKER="./LAST_PATCHED_AT"
+
+MOVE /Y "%WARTALE_HOME%\assets.pak.back" "%WARTALE_HOME%\assets.pak"
+MOVE /Y "%WARTALE_HOME%\res.pak.back" "%WARTALE_HOME%\res.pak"
+
+DEL %LAST_PATCHED_TRACKER%
