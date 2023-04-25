5f5762e978513ddc4e7a9b9167d7c71a53aa176c
Eunchul <eunchulsong9@gmail.com>
2023-04-25 21:49:23 +0900

    pause console even if error occured. open wartale home select prompt

--- patch.bat
+++ patch.bat
@@ -1,40 +1,52 @@
-@echo off
-chcp 65001
+@echo OFF
+SETLOCAL enabledelayedexpansion
+CHCP 65001
 
-SET "WARTALE_HOME=C:\Program Files (x86)\Steam\steamapps\common\Wartales"
+SET "WARTALE_HOME=%ProgramFiles(x86)%\Steam\steamapps\common\Wartales"
+SET "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Wartales 디렉토리를 찾을수 없습니다. Wartales가 설치된 디렉토리를 직접 선택해주세요',0,0).self.path"" || goto :error
+
+IF NOT EXIST "%WARTALE_HOME%" (
+  echo Wartales 디렉토리를 찾을수 없습니다. 프롬프트에서 Wartales가 설치된 디렉토리를 직접 선택해주세요
+  FOR /f "usebackq delims=" %%I IN (`powershell %psCommand%`) DO SET "WARTALE_HOME=%%I"
+) || goto :error
+
 SET "LAST_PATCHED_TRACKER=%WARTALE_HOME%\__LAST_KR_PATCHED_AT"
 
-SET LAST_PATCHED=null
 IF EXIST "%LAST_PATCHED_TRACKER%" (
   FOR /f "delims=" %%a in ('type "%LAST_PATCHED_TRACKER%"') DO set "LAST_PATCHED=%%a"
-) || EXIT /B 1
+)
 
-FOR %%a IN ("%WARTALE_HOME%\res.pak") do set "LAST_MODIFIED=%%~ta" || EXIT /B 1
+FOR %%a IN ("%WARTALE_HOME%\res.pak") do set "LAST_MODIFIED=%%~ta" || goto :error
 
 IF "%LAST_PATCHED%" NEQ "%LAST_MODIFIED%" (
   echo(
   echo The last patch time is not equal to the asset modified time. 
-  echo %LAST_PATCHED% != %LAST_MODIFIED%
+  echo %LAST_PATCHED% is not same as %LAST_MODIFIED%
   echo Need to backup the asset files.
-  echo Try to backup "assets.pak" into "assets.pak.back"...
+  echo(
+  echo Try to backup "assets.pak" into "assets.pak.back". Please wait a minute...
   copy "%WARTALE_HOME%\assets.pak" "%WARTALE_HOME%\assets.pak.back"
-  echo Try to backup "res.pak" into "res.pak.back"...
+  echo(
+  echo Try to backup "res.pak" into "res.pak.back". Please wait a minute...
   copy "%WARTALE_HOME%\res.pak" "%WARTALE_HOME%\res.pak.back"
+  echo(
   echo Backup is done.
-) || EXIT /B 1
+) || goto :error
 
 IF NOT EXIST quickbms (
   echo quickbms is not found. Try to download it..
   curl -O http://aluigi.altervista.org/papers/quickbms.zip
   mkdir quickbms
   tar -C quickbms -xf quickbms.zip
-) || EXIT /B 1
+) || goto :error
 
 echo(
 echo Try to modify the assets.pak..
-quickbms\quickbms_4gb_files.exe -w -r dune_spice_wars_extract.bms "%WARTALE_HOME%\assets.pak" asset || EXIT /B 1
+quickbms\quickbms_4gb_files.exe -w -r dune_spice_wars_extract.bms "%WARTALE_HOME%\assets.pak" asset || goto :error
+echo(
 echo Try to modify the res.pak..
-quickbms\quickbms.exe -w -r -r dune_spice_wars_extract.bms "%WARTALE_HOME%\res.pak" res || EXIT /B 1
+quickbms\quickbms.exe -w -r -r dune_spice_wars_extract.bms "%WARTALE_HOME%\res.pak" res || goto :error
 
 echo(
 echo Touch the patch time tracker..
@@ -43,6 +55,15 @@ echo %LAST_PATCHED%> "%LAST_PATCHED_TRACKER%"
 
 echo(
 echo Patch done!
+echo(
+
+pause
+goto :EOF
+
+
+:error
+echo(
+echo Failed with error #%errorlevel%.
 echo(
 
 pause
